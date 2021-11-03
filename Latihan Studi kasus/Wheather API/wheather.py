50e984c42814a572cfb9e746eea62cd114e65aba
iqbalfadhila <iqbalfadhila8902@gmail.com>
2021-11-03 11:41:26 +0700

    QR Code, Wheather SnakeGame

---
+++ Latihan Studi kasus/Wheather API/wheather.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+import requests
+import time
+
+
+def getwheathe():
+    city = textfield.get()
+
+
+canvas = tk.Tk()
+canvas.geometry('600x500')
+canvas.title('Weather App')
+
+f = ('poppins', 15, 'bold')
+t = ('poppins', 35, 'bold')
+
+textfield = tk.Entry(canvas, font=t)
+textfield.pack(pady=20)
+textfield.focus()
+
+label1 = tk.Label(canvas, font=t)
+label1.pack()
+label2 = tk.Label(canvas, font=f)
+label2.pack()
+
+canvas.mainloop()
